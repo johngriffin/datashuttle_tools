9b123d95f415a271c15a73d2644274a9879e0093
Rob Hardwick <robhardwick@gmail.com>
2011-09-15 23:48:27 +0100

    Add LAD latitude and longitude to Solr import script

--- scripts/solr_import/import.php
+++ scripts/solr_import/import.php
@@ -13,11 +13,17 @@ $config = array(
 		'name'	=> 'datashuttle',
 	),
 	'solr' => array(
-		'host'	=> 'localhost',
+		'host'	=> '127.0.0.1',
 		'port'	=> 8983,
 		'path'	=> '/solr',
 	),
+	'kasabi' => array(
+		'url'	=> 'http://api.kasabi.com/dataset/ordnance-survey-linked-data/apis/sparql',
+		'key'	=> '503fd03ef47dfd89b8470301692732332828dba3',
+	),
 );
+
+$lads = array();
 
 require_once('lib/Apache/Solr/Service.php');
 require_once('lib/Apache/Solr/HttpTransport/Curl.php');
@@ -37,6 +43,49 @@ if (!$solr->ping()) {
 	die("Unable to connect to SOLR\n");
 }
 
+// Fetch LADs
+print "Fetching LADs...";
+
+$url = sprintf('%s?%s',
+	$config['kasabi']['url'],
+	http_build_query(array(
+		'apikey' => $config['kasabi']['key'],
+		'output' => 'json',
+		'query' => "
+			select distinct ?label ?census ?lat ?long
+			where {
+				?s <http://data.ordnancesurvey.co.uk/ontology/admingeo/hasCensusCode> ?census .
+				?s <http://www.w3.org/2004/02/skos/core#altLabel> ?label .
+				?s <http://www.w3.org/2003/01/geo/wgs84_pos#lat> ?lat .
+				?s <http://www.w3.org/2003/01/geo/wgs84_pos#long> ?long .
+			}"
+	))
+);
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HEADER, false);
+
+$data = curl_exec($ch);
+curl_close($ch);
+
+$data = json_decode($data, true);
+if (is_null($data) || !isset($data['results']['bindings'])) {
+	die("Unable to parse JSON data");
+}
+
+foreach($data['results']['bindings'] as $lad) {
+	$lads['H' . $lad['census']['value']] = array(
+		'label'	=> $lad['label']['value'],
+		'lat'	=> $lad['lat']['value'],
+		'lng'	=> $lad['long']['value'],
+	);
+}
+
+unset($data);
+print "done\n\n";
+
 // Delete existing index
 $solr->deleteByQuery('*:*');
 
@@ -52,27 +101,36 @@ $count = 0;
 // Iterate over DB rows adding each one as a SOLR doc
 while ($row = $result->fetch_assoc()) {
 
-	// Extract area ID from row ID
+	// Extract LAD from row ID
 	if (!preg_match('#^http://datashuttle\.org/mortality/[\w\d-%.]+/([\w\d]+)/\d+/[MF]$#', $row['id'], $matches)) {
 		printf("ERROR: Couldn't parse ID: %s\n", $row['id']);
 		continue;
 	}
 
+	// Lookup LAD
+	$lad = $lads[$matches[1]];
+
+	if (!isset($lad)) {
+		print "ERROR: Couldn't find LAD: $matches[1]\n";
+		continue;
+	}
+
 	// Create document
 	$doc = new Apache_Solr_Document();
-	$doc->id		= $row['id'];
-	$doc->icd		= $row['icdcode'];
-	$doc->area		= $matches[1];
-	$doc->area_name	= $row['areaname'];
-	$doc->year		= $row['year'];
-	$doc->gender	= $row['gender'];
-	$doc->value		= $row['value'];
+	$doc->id			= $row['id'];
+	$doc->icd			= $row['icdcode'];
+	$doc->area			= $matches[1];
+	$doc->area_name		= $lad['label'];
+	$doc->area_location	= $lad['lat'] . ',' . $lad['lng'];
+	$doc->year			= $row['year'];
+	$doc->gender		= $row['gender'];
+	$doc->value			= $row['value'];
 
 	// Add document to index
 	try {
 		$solr->addDocument($doc);
 	} catch (Exception $e) {
-		print "ERROR: Couldn't add row\n";
+		print "ERROR: Couldn't add row: " . $e->getMessage() . "\n";
 		continue;
 	}
 
